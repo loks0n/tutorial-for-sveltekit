53382b40d1aac41113cded7d9fd14f7c5d9987de
loks0n <22452787+loks0n@users.noreply.github.com>
2024-01-23 11:30:05 +0000

    fix: oauth

--- src/lib/oauth.ts
+++ src/lib/oauth.ts
@@ -2,7 +2,7 @@ import { browser } from '$app/environment';
 import { goto } from '$app/navigation';
 import { PUBLIC_APPWRITE_ENDPOINT } from '$env/static/public';
 
-export function signInWithGithubUrl() {
+export function signInWithGithub() {
 	if (!browser) return;
 
 	const url = new URL(`${PUBLIC_APPWRITE_ENDPOINT}/account/sessions/oauth2/github`);
